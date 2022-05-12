1fccb31c0562862fc398d841b7e55b7582e452ec
allan <allangreen2906@gmail.com>
2022-05-11 22:16:48 -0300

    feat: added styles in formulary

---
+++ src/components/Form/ResultImc/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  formContext: {
+    width: '100%',
+    height: '100%',
+    bottom: 0,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    marginTop: 30,
+  },
+  form: {
+      width: '100%',
+      height: 'auto',
+      marginTop: 30,
+      padding: 10,
+  }
+});
